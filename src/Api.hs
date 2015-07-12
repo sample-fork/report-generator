d95a7e61cdb14308cddecdb528378413d5a077d8
Elliot Stokes <elliot.stokes@gmail.com>
2015-07-12 22:47:18 +0100

    add create report endpoint

--- src/Api.hs
+++ src/Api.hs
@@ -7,25 +7,31 @@ import           Snap.Core
 import           Data.Aeson
 import           Snap.Snaplet
 import           DataTypes
+import qualified Data.ByteString.Lazy.Char8 as L
 import qualified Data.ByteString.Char8 as B
 
 
 apiRoutes :: [(B.ByteString, Handler b Api ())]
-apiRoutes = [ ("echo/:echoparam",  method GET respondOk)
-             ,("reports",  method GET getReports)
+apiRoutes = [ ("reports",  method GET getReports)
              ,("reports/:reportId", method GET getReport)
+             ,("reports", method POST createReport)
             ]
+
+notFound :: Handler b Api ()
+notFound = do
+  modifyResponse . setResponseCode $ 404
+  writeLBS "Not Found"
+
+badRequest :: Handler b Api()
+badRequest = do
+  modifyResponse . setResponseCode $ 400
+  writeLBS "Bad Request"
 
 getReports :: Handler b Api ()
 getReports = do
   reports <- query_ "SELECT id, name, description,effort FROM \"Reports\""
   modifyResponse $ setHeader "Content-Type" "application/json"
   writeLBS . encode $ (reports :: [Report])
-
-notFound :: Handler b Api ()
-notFound = do
-  modifyResponse . setResponseCode $ 404
-  writeLBS "Not Found"
 
 getReport :: Handler b Api ()
 getReport = do
@@ -37,14 +43,17 @@ getReport = do
     else writeLBS . encode $ head (reports :: [Report])
 
 
+createReport :: Handler b Api ()
+createReport = do
+  report <- readRequestBody 65536
+  modifyResponse $ setHeader "Content-Type" "application/json"
+  case (decode report :: Maybe Report) of
+    Just x -> do
+      execute "INSERT INTO \"Reports\" (name) VALUES (?)" [name x]
+      writeLBS . encode $ x
+    Nothing -> badRequest
   
-
-respondOk :: Handler b Api ()
-respondOk = do
-  param <- getParam "echoparam"
-  maybe (writeBS "must specify echo/param in URL")
-         writeBS param
-  modifyResponse . setResponseCode $ 200
+  
 
 apiInit :: SnapletInit b Api
 apiInit = makeSnaplet "api" "Api Endpoints" Nothing $ do
--- src/DataTypes.hs
+++ src/DataTypes.hs
@@ -10,13 +10,16 @@ import           Database.PostgreSQL.Simple.ToRow
 import           Database.PostgreSQL.Simple.ToField
 import           Control.Monad.State.Class
 import           Snap.Snaplet
+import           Control.Monad.Reader
+import           Control.Applicative
+import           Control.Monad
 import           Data.Aeson as A
 import           Snap.Snaplet.Heist
 import           Snap.Snaplet.PostgresqlSimple
 
 data Report = Report { reportId :: Int
                      , name :: String
-                     , description :: String
+                     , description :: Maybe String
                      , effort :: Int} deriving (Show)
 
 instance FromRow Report where
@@ -28,12 +31,22 @@ instance ToRow Report where
 instance ToJSON Report where
   toJSON (Report reportId name description effort) = object [ "id" A..= reportId, "name" A..= name, "description" A..= description, "effort" A..= effort ]
 
+instance FromJSON Report where
+  parseJSON (Object v) = Report <$>
+                         v .: "id" <*>
+                         v .: "name" <*>
+                         v .:? "description" <*>
+                         v .: "effort"
+  parseJSON _ = mzero
+
+
 data Api = Api { _pg :: Snaplet Postgres }
 
 makeLenses ''Api
 
 instance HasPostgres (Handler b Api) where
   getPostgresState = with pg get
+  setLocalPostgresState s = local (set (pg . snapletValue) s)
 
 data App = App { _heist :: Snaplet (Heist App), _api :: Snaplet Api}
 
